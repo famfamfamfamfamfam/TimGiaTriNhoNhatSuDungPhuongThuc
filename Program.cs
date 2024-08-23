99bd0715014d2e6772091885db32abf9ae348809
BinhTran <fam.out16@gmail.com>
2024-08-23 21:23:32 +0700

    Add cac files cua bai thuc hanh Tim gia tri nho nhat su dung phuong thuc

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace SUDUNGPHUONGTHU
+{
+    class TimGiaTriNhoNhatMang
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so luong phan tu mang:");
+            int sophantu = Convert.ToInt32(Console.ReadLine());
+            int[] mangnhapvao = new int[sophantu];
+            Console.WriteLine("Nhap cac phan tu:");
+            for(int chiso = 0; chiso < sophantu; chiso++)
+            {
+                mangnhapvao[chiso] = Convert.ToInt32(Console.ReadLine());
+            }
+            int vitriphantu = MinValue(mangnhapvao);
+            Console.WriteLine($"Phan tu nho nhat cua mang la: {mangnhapvao[vitriphantu]}");
+        }
+        public static int MinValue(int [] mang)
+        {
+            int giatrinhonhat = mang[0];
+            int chisonhonhat = 0;
+            for(int chiso=1; chiso < mang.Length; chiso++)
+            {
+                if (mang[chiso]<giatrinhonhat)
+                {
+                    giatrinhonhat = mang[chiso];
+                    chisonhonhat = chiso;
+                }
+            }
+            return chisonhonhat;
+        }
+    }
+}
